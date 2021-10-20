73b2fd9a472aebe602c56bee450e05bc21330e9d
Om Mate <ommate13@gmail.com>
2021-10-20 21:55:52 +0530

    reer

--- portfolio.php
+++ portfolio.php
@@ -4,12 +4,13 @@ if($connection){
     echo "connection is established";
 }
 else{
-    echo"error connection failed";
+    echo "error connection failed";
 }
-mysqli_select_db("$connection,'portfolio');
-$nam=$_post['_name'];
-$emailbeta=$_post['_email'];
-$messagekar=$_post['_message'];
+mysqli_select_db($connection,'portfolio');
+$nam=$_POST['_name'];
+$emailbeta=$_POST['_email'];
+$messagekar=$_POST['_message'];
 $data="INSERT INTO 2021resume (name,email,message) VALUES('$nam','$emailbeta','$messagekar')";
 mysqli_query($connection,$data);
-header('location:signupscc.php');
+header('location:signupsucc.php');
+?>
