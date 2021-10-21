cdf5a220fd09e45c9ef67017179e85fb51c2028b
Joel Silas <67523178+joelsilas1816@users.noreply.github.com>
2021-10-21 19:36:32 +0530

    new

--- portfolio.php
+++ portfolio.php
@@ -6,15 +6,6 @@ if($connection){
 else{
     echo "error connection failed";
 }
-<<<<<<< HEAD
-mysqli_select_db("$connection,'portfolio');
-$nam=$_post['_name'];
-$emailbeta=$_post['_email'];
-$messagekar=$_post['_message'];
-$data="INSERT INTO 2021resume (name,email,message) VALUES('$nam','$emailbeta','$messagekar')";
-mysqli_query($connection,$data);
-header('location:signupscc.php');
-=======
 mysqli_select_db($connection,'portfolio');
 $nam=$_POST['name'];
 $emailbeta=$_POST['email'];
@@ -28,5 +19,4 @@ if(!$result){
 }
 echo "Success";
 mysqli_close($connection);
->>>>>>> 6128bd7f7184db7cfed5e84757688b51dbec4d2b
 ?>
