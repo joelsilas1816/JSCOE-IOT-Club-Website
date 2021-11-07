e5da530d94a6eec79cc3c41ddc1ce237dcc4397a
joelsilas1816 <silasjoel03@gmail.com>
2021-11-08 01:29:13 +0530

    changes

--- assets/js/myjs.js
+++ assets/js/myjs.js
@@ -1,5 +1,5 @@
 //Get the button
-mybutton = document.getElementById("myBtn");
+let mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
@@ -24,26 +24,42 @@ let mybutton1 = document.getElementById("dtype1");
 let mybutton2 = document.getElementById("dtype2");
 let mybutton3 = document.getElementById("dtype3");
 
-let mydesc1 = document.getElementById("desctype1")
-let mydesc2 = document.getElementById("desctype2")
-let mydesc3 = document.getElementById("desctype3")
+let mydesc1 = document.getElementById("desctype1");
+let mydesc2 = document.getElementById("desctype2");
+let mydesc3 = document.getElementById("desctype3");
+
+let ip1 = document.getElementById("itypep1");
+let ip2 = document.getElementById("itypep2");
+let ip3 = document.getElementById("itypep3");
+
+let im1 = document.getElementById("itypem1");
+let im2 = document.getElementById("itypem2");
+let im3 = document.getElementById("itypem3");
 
 mybutton1.onclick = function () {
 
     if (mydesc1.style.display !== "none") {
         mydesc1.style.display = "none";
+        ip1.style.display = "inline-block";
     }
-    else if (mydesc1.style.display = "none") {
+    else if (mydesc1.style.display == "none") {
             mydesc1.style.display = "block";
+            im1.style.display = "inline-block";
+
         }
 }
 mybutton2.onclick = function () {
 
     if (mydesc2.style.display !== "none") {
         mydesc2.style.display = "none";
+        ip2.style.display = "inline-block";
+        ip2.style.display = "inline-block";
+
     }
-    else if (mydesc2.style.display = "none") {
+    else if (mydesc2.style.display == "none") {
             mydesc2.style.display = "block";
+            im2.style.display = "inline-block";
+
         }
 
 
@@ -52,9 +68,14 @@ mybutton3.onclick = function () {
 
     if (mydesc3.style.display !== "none") {
         mydesc3.style.display = "none";
+        ip3.style.display = "inline-block";
+        ip3.style.display = "inline-block";
+
     }
-    else if (mydesc3.style.display = "none") {
+    else if (mydesc3.style.display == "none") {
             mydesc3.style.display = "block";
+            im3.style.display = "inline-block";
+
         }
 
 }
